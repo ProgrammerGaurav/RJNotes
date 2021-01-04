1c502892ae05546a1576184921be03c845452e13
programmergaurav <gauravgupta14376@gmail.com>
2021-01-04 20:31:01 +0530

    Legacy Code

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const navbarNav = document.querySelector('.navbar-nav');
+const toggle = document.querySelector('.toggle');
+const navbar = document.querySelector('.navbar')
+
+toggle.addEventListener('click', () => {
+    navbarNav.classList.toggle('show');
+});
+
+
+const menus = document.querySelectorAll('.menu-item')
+menus.forEach(menu => {
+    menu.querySelector('.menu-title').addEventListener('click', () => {
+        menu.classList.toggle('active')
+        const panel = menu.querySelector('.menu-context');
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+});
+const sidebarToggle = document.querySelector('.sidebar-toggle');
+const sidebar = document.querySelector('.menu');
+const context = document.querySelector('.context');
+sidebarToggle.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+    context.classList.toggle('active')
+
+})
